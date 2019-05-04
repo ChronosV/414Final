dfb37fd065140f082f318b4e360c08e0948e86ae
Taggerung559 <32943131+Taggerung559@users.noreply.github.com>
2019-05-04 13:38:15 -0400

    Add files via upload

---
+++ 414FinalProject.m
@@ -0,0 +1,16 @@
+%Clear things up
+clear all; clc; 
+clf;
+%Get a TF
+s = tf('s');
+
+%Create constants
+Jg = 6.2e-6;    %Gearbox inertia
+Js = 1.4e-7;    %Angular sensor inertia
+Gv = 5;         %Voltage amplifier gain
+Ks = 0.1e-3;    %touch sensor, in V/m
+Ra = 1e-2;      %Radius of ball, in meters
+Rh = 6e-3;      %Height difference between center of ball and top of rail, in meters
+Jb = 0.4*Ra*Ra  %Ball inertia, neglecting the mass
+G = 9.81        %Acceleration of gravity
+
