f4eadfdcb1f002b857171dc2023d9c1b69e35dc0
Ethan Knowles <eknowl31@gmail.com>
2019-05-04 13:52:11 -0400

    Update MotorData.m

--- MotorData.m
+++ MotorData.m
@@ -7,28 +7,36 @@ Motor = struct();
 if num==1
     Jm = 5.0e-5;
     Bm = 3.0e-6;
-    Kt = 0.225;
+    Kt(1) = 0.225;  %nominal
+    Kt(2) = 0.2025; %min
+    Kt(3) = 0.2475; %max
     Re = 8;
     L = 25e-3;
 
 elseif num==2
     Jm = 5.0e-5;
     Bm = 3.0e-6;
-    Kt = 0.175;
+    Kt(1) = 0.175;  %nominal
+    Kt(2) = 0.1575; %min
+    Kt(3) = 0.1925; %max
     Re = 6;
     L = 16e-3;
 
 elseif num==3
     Jm = 5.0e-5;
     Bm = 3.0e-6;
-    Kt = 0.125;
+    Kt(1) = 0.125;  %nominal
+    Kt(2) = 0.1125; %min
+    Kt(3) = 0.1375; %max
     Re = 4;
     L = 7.5e-3;
 
 elseif num==2
     Jm = 5.0e-5;
     Bm = 3.0e-6;
-    Kt = 0.275;
+    Kt(1) = 0.275;  %nominal
+    Kt(2) = 0.2475; %min
+    Kt(3) = 0.3025; %max
     Re = 12;
     L = 32e-3;
 end
