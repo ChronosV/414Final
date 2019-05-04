e622f8c6cac584a36d435af1ffcc7dca1fa84291
Ethan Knowles <eknowl31@gmail.com>
2019-05-04 15:11:18 -0400

    Update 414FinalProject.m

--- 414FinalProject.m
+++ 414FinalProject.m
@@ -11,6 +11,6 @@ Gv = 5;         %Voltage amplifier gain
 Ks = 10;    %touch sensor, in V/m
 Ra = 1e-2;      %Radius of ball, in meters
 Rh = 6e-3;      %Height difference between center of ball and top of rail, in meters
-Jb = 0.4*Ra*Ra; %Ball inertia, neglecting the mass
+Jb = 4.52185e-7; %Ball inertia
 G = 9.81;       %Acceleration of gravity
 N = 10;         %Chosen Gear Ratio
