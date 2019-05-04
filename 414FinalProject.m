a0a9ce55a50e64b86a01e572906679c4babe5dce
Ethan Knowles <eknowl31@gmail.com>
2019-05-04 15:38:15 -0400

    Update 414FinalProject.m

--- 414FinalProject.m
+++ 414FinalProject.m
@@ -5,12 +5,14 @@ clf;
 s = tf('s');
 
 %Create constants
-Jg = 6.2e-6;    %Gearbox inertia
-Js = 1.4e-7;    %Angular sensor inertia
-Gv = 5;         %Voltage amplifier gain
-Ks = 10;    %touch sensor, in V/m
-Ra = 1e-2;      %Radius of ball, in meters
-Rh = 6e-3;      %Height difference between center of ball and top of rail, in meters
-Jb = 4.52185e-7; %Ball inertia
-G = 9.81;       %Acceleration of gravity
-N = 10;         %Chosen Gear Ratio
+Jg = 6.2e-6;        %Gearbox inertia
+Js = 1.4e-7;        %Angular sensor inertia
+Gv = 5;             %Voltage amplifier gain
+Ks = 10;            %touch sensor, in V/m
+Rb = 1e-2;          %Radius of ball, in meters
+Rh = 6e-3;          %Height difference between center of ball and top of rail, in meters
+Mb = 1.1305e-2;     %Mass of ball
+Jb = 0.4*Mb*(Rb)^2; %Ball inertia
+G = 9.81;           %Acceleration of gravity
+N = 10;             %Chosen Gear Ratio
+Jx = Jg+Js+Jb;      %Constant inertia, neglecting motor
