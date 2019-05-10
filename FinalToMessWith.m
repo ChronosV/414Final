f5caf8c0f27c85e2193a6f8b6840df9ea3aa7665
Ethan Knowles <eknowl31@gmail.com>
2019-05-09 20:32:42 -0400

    more commented out
    Commented out some more unused code

--- FinalToMessWith.m
+++ FinalToMessWith.m
@@ -381,52 +381,52 @@ legend('Kt = 0.2025', 'Kt = 0.225','Kt = 0.2475', 'Location', 'northeast');
 
 %% Input to Current Transfer Function Characteristics for Motor 4
 % Find the input to current path for I(s)/R(s)
-C_4_zpk = zpk(C_4);
-N = 10;                     % Set N = 10
-for i = 1:length(Kt_4)         
-    s4 = tf([1 0 0 0 0],[1]);   % make a s^4 term
-    Is = ((K*C_4.*Gv)/L_4)*s4;  % Numerator for I(s)/R(s)
-    Rs1 = tf([1, (R_4/L_4 + Bm./J_eff_10), ((Kt_4(i)*Kt_4(i))./(L_4*J_eff_10) + (R_4*Bm)./(L_4.*J_eff_10)), 0, 0, 0],[1]);
-    Rs2 = tf([((C_4.*Gv*Kt_4(i)*g*Kv)/(L_4*J_eff_10*N*M))],[1]);
-    Rs = Rs1 - Rs2;             % Denominator for I(s)/R(s)
-    IRS(i) = Is/Rs;                % Find I(s)/R(s)
-    IRS_Simplify4 = minreal(IRS) % Simplify for use in report
-    figure(18)
-    hold on
-    lsim(IRS(i),Pos1(t),t)               % Find the response to the desired input
-    title('Input to Current Response Motor 4');
-    hold off
-end
-legend('Kt = 0.1125', 'Kt = 0.125','Kt = 0.1375', 'Location', 'northeast');
+%C_4_zpk = zpk(C_4);
+%N = 10;                     % Set N = 10
+%for i = 1:length(Kt_4)         
+%    s4 = tf([1 0 0 0 0],[1]);   % make a s^4 term
+%    Is = ((K*C_4.*Gv)/L_4)*s4;  % Numerator for I(s)/R(s)
+%    Rs1 = tf([1, (R_4/L_4 + Bm./J_eff_10), ((Kt_4(i)*Kt_4(i))./(L_4*J_eff_10) + (R_4*Bm)./(L_4.*J_eff_10)), 0, 0, 0],[1]);
+%    Rs2 = tf([((C_4.*Gv*Kt_4(i)*g*Kv)/(L_4*J_eff_10*N*M))],[1]);
+%    Rs = Rs1 - Rs2;             % Denominator for I(s)/R(s)
+%    IRS(i) = Is/Rs;                % Find I(s)/R(s)
+%    IRS_Simplify4 = minreal(IRS) % Simplify for use in report
+%    figure(18)
+%    hold on
+%    lsim(IRS(i),Pos1(t),t)               % Find the response to the desired input
+%    title('Input to Current Response Motor 4');
+%    hold off
+%end
+%legend('Kt = 0.1125', 'Kt = 0.125','Kt = 0.1375', 'Location', 'northeast');
 
 
 %% Input to phase Transfer Function Characteristics for Motor 4
 % Find the input to phase path for O(s)/R(s)
-for i = 1:length(Kt_4)    
-    s2 = tf([1 0 0],[1]);       % make a s^2 term
-    Os = ((K*C_4*Gv*Kt_4)/(L_4*J_eff_10*N))*s2;     % Numerator for O(s)/R(s)   
-    ORS = Os/Rs;                % Find O(s)/R(s)
-    ORS_Simplify4 = minreal(ORS) % Simplify fo use in report
-    figure(19)
-    hold on
-    lsim(ORS(i),Pos1(t),t)               % Find the response to the desired input
-    title('Input to Phase Response Motor 4');
-    hold off
-end
-legend('Kt = 0.1125', 'Kt = 0.125','Kt = 0.1375', 'Location', 'northeast');
-
+%for i = 1:length(Kt_4)    
+%    s2 = tf([1 0 0],[1]);       % make a s^2 term
+%    Os = ((K*C_4*Gv*Kt_4)/(L_4*J_eff_10*N))*s2;     % Numerator for O(s)/R(s)   
+%    ORS = Os/Rs;                % Find O(s)/R(s)
+%    ORS_Simplify4 = minreal(ORS) % Simplify fo use in report
+%    figure(19)
+%    hold on
+%    lsim(ORS(i),Pos1(t),t)               % Find the response to the desired input
+%    title('Input to Phase Response Motor 4');
+%    hold off
+%end
+%legend('Kt = 0.1125', 'Kt = 0.125','Kt = 0.1375', 'Location', 'northeast');
+%
 
 %% Input to Output Transfer Function Characteristics for Motor 4
 % Find the input to phase path for Y(s)/R(s)
-for i = 1:length(Kt_4)    
-    Ys = ((K*C_4*Gv*Kt_4*-g*Kv)/(L_4*J_eff_10*N*M));     % Numerator for Y(s)/R(s)   
-    YRS = Ys/Rs;                % Find Y(s)/R(s)
-    YRS_Simplify4 = minreal(YRS) % Simplify fo use in report
-    figure(20)
-    hold on
-    lsim(YRS(i),Pos1(t),t)               % Find the response to the desired input
-    title('Input to Output Response Motor 4');
-    hold off
-end
-legend('Kt = 0.1125', 'Kt = 0.125','Kt = 0.1375', 'Location', 'northeast');
+%for i = 1:length(Kt_4)    
+%    Ys = ((K*C_4*Gv*Kt_4*-g*Kv)/(L_4*J_eff_10*N*M));     % Numerator for Y(s)/R(s)   
+%    YRS = Ys/Rs;                % Find Y(s)/R(s)
+%    YRS_Simplify4 = minreal(YRS) % Simplify fo use in report
+%    figure(20)
+%    hold on
+%    lsim(YRS(i),Pos1(t),t)               % Find the response to the desired input
+%    title('Input to Output Response Motor 4');
+%    hold off
+%end
+%legend('Kt = 0.1125', 'Kt = 0.125','Kt = 0.1375', 'Location', 'northeast');
 
